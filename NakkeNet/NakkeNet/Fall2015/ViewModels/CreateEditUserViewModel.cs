a4c6eca263e357f0ad9138f60c9b0d134afb53e5
Kenneth <kennethjespersen88@gmail.com>
2015-12-10 18:01:22 +0100

    minor

--- NakkeNet/NakkeNet/Fall2015/ViewModels/CreateEditUserViewModel.cs
+++ NakkeNet/NakkeNet/Fall2015/ViewModels/CreateEditUserViewModel.cs
@@ -12,7 +12,7 @@ namespace NakkeNet.ViewModels
         public User User { get; set; }
         public List<CompetencyHeader> CompetencyHeaders { get; set; }
         //public List<Competency> Competencies { get; set; }
-        //public IEnumerable<Competency> Competencies { get; set; }
+        public IEnumerable<Competency> Competencies { get; set; }
         //public ICollection<Competency> Competencies { get; set; }
         
 
